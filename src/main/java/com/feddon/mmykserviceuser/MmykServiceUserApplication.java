0a805fe78f1f30ab57c165ba3548f2b52bf16414
89360944@qq.com <89360944@qq.com>
2022-04-10 13:07:13 +0800

    test

--- src/main/java/com/feddon/mmykserviceuser/MmykServiceUserApplication.java
+++ src/main/java/com/feddon/mmykserviceuser/MmykServiceUserApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class MmykServiceUserApplication {
 
     public static void main(String[] args) {
-        System.out.println("hello MmykServiceUserApplication");
+        System.out.println("hello MmykServiceUserApplication1");
         SpringApplication.run(MmykServiceUserApplication.class, args);
     }
 
