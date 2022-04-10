bf3f98d50b2e70a6b94542dff82902f35c71b420
89360944@qq.com <89360944@qq.com>
2022-04-10 21:08:37 +0800

    test

--- src/main/java/com/feddon/mmykserviceuser/controller/SiteController.java
+++ src/main/java/com/feddon/mmykserviceuser/controller/SiteController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "站点-Site")
-@RestController("/site")
+@RestController("")
 public class SiteController {
 
     @Autowired
