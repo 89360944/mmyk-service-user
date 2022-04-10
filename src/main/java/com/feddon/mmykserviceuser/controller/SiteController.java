79b8de00a206010dce0ce9e89b20eacaa834ffde
89360944@qq.com <89360944@qq.com>
2022-04-10 22:09:08 +0800

    test

--- src/main/java/com/feddon/mmykserviceuser/controller/SiteController.java
+++ src/main/java/com/feddon/mmykserviceuser/controller/SiteController.java
@@ -9,16 +9,24 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "站点-Site")
-@RestController("")
+import javax.servlet.http.HttpServletRequest;
+
+@Tag(name = "站点--Site")
+@RestController
 public class SiteController {
 
     @Autowired
     private SiteService siteService;
+
+    @GetMapping("/test")
+    public String test(HttpServletRequest request){
+        return "hello world, uri:" + request.getRequestURI();
+    }
 
     @Operation(summary = "创建站点")
     @PostMapping(value = "/createSite")
